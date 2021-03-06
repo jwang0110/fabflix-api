6fdd6b5d629da177db290dd2a9dba618a53f1204
jwang0110 <johnw7@uci.edu>
2021-03-06 01:01:13 -0800

    added link for latest movie

---
+++ controllers/movieLatest.js
@@ -0,0 +1,16 @@
+const handleMovieLatest = async (req, res, moviedb) => {
+	const params = {
+		language: "en-US",
+	};
+
+	try {
+		const response = await moviedb.movieLatest(params);
+		res.json(response);
+	} catch (e) {
+		res.status(404).json(e);
+	}
+};
+
+module.exports = {
+	handleMovieLatest,
+};
--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const { MovieDb } = require("moviedb-promise");
+
+const movieLatest = require("./controllers/movieLatest.js");
+
+const moviedb = new MovieDb(process.env.TMDB_API_KEY);
 
 const app = express();
 app.use(express.json());
@@ -7,6 +12,10 @@ app.use(cors());
 
 app.get("/", (req, res) => {
 	res.send("fabflix-api is working");
+});
+
+app.get("/latest", async (req, res) => {
+	movieLatest.handleMovieLatest(req, res, moviedb);
 });
 
 app.listen(process.env.PORT || 3001, () => {
