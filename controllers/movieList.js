11c0c1a4024600e759ffba84cbe1355ecc8277bf
jwang0110 <johnw7@uci.edu>
2021-03-07 23:56:25 -0800

    combined links and filtered data

---
+++ controllers/movieList.js
@@ -0,0 +1,103 @@
+const params = {
+	language: "en-US",
+};
+
+const fetchMovies = async (moviedb, sorted) => {
+	let response = null;
+
+	try {
+		switch (sorted) {
+			case "now_playing":
+				response = await moviedb.movieNowPlaying(params);
+				break;
+			case "popular":
+				response = await moviedb.moviePopular(params);
+				break;
+			case "top_rated":
+				response = await moviedb.movieTopRated(params);
+				break;
+			case "upcoming":
+				response = await moviedb.upcomingMovies(params);
+				break;
+			default:
+				response = null;
+		}
+
+		return response;
+	} catch (e) {
+		throw e;
+	}
+};
+
+const getMovie = (movie) => {
+	return {
+		id: movie.id,
+		title: movie.title,
+		date: movie.date,
+		rating: movie.vote_average,
+	};
+};
+
+const getStars = (credits) => {
+	const stars = credits.cast.map((star) => {
+		return {
+			id: star.id,
+			name: star.name,
+		};
+	});
+
+	return stars;
+};
+
+const getDirector = (credits) => {
+	const director = credits.crew.find((person) => person.job === "Director");
+
+	if (!director) {
+		return null;
+	} else {
+		return {
+			id: director.id,
+			name: director.name,
+		};
+	}
+};
+
+const handleMovieList = async (req, res, moviedb) => {
+	const { sorted } = req.params;
+
+	try {
+		const movies = await fetchMovies(moviedb, sorted);
+
+		if (!movies) {
+			res.status(404).json("Page Not Found");
+			return;
+		}
+
+		const filteredMovies = await Promise.all(
+			movies.results.map(async (m) => {
+				const movie_id = m.id;
+				const genre_ids = m.genre_ids;
+				const credits = await moviedb.movieCredits(movie_id);
+				const movie = getMovie(m);
+				const stars = getStars(credits);
+				const director = getDirector(credits);
+
+				return {
+					movie,
+					genre_ids,
+					stars,
+					director,
+				};
+			})
+		);
+
+		res.json(filteredMovies);
+	} catch (e) {
+		console.log(e);
+		res.status(404).json(e);
+	}
+};
+
+module.exports = {
+	handleMovieList,
+};
--- server.js
+++ server.js
@@ -3,10 +3,7 @@ const cors = require("cors");
 const { MovieDb } = require("moviedb-promise");
 
 const movieLatest = require("./controllers/movieLatest.js");
-const movieNowPlaying = require("./controllers/movieNowPlaying.js");
-const moviePopular = require("./controllers/moviePopular.js");
-const movieTopRated = require("./controllers/movieTopRated.js");
-const movieUpcoming = require("./controllers/movieUpcoming.js");
+const movieList = require("./controllers/movieList.js");
 
 const moviedb = new MovieDb(process.env.TMDB_API_KEY);
 
@@ -22,20 +19,8 @@ app.get("/movie/latest", async (req, res) => {
 	movieLatest.handleMovieLatest(req, res, moviedb);
 });
 
-app.get("/movie/now_playing", async (req, res) => {
-	movieNowPlaying.handleMovieNowPlaying(req, res, moviedb);
-});
-
-app.get("/movie/popular", async (req, res) => {
-	moviePopular.handleMoviePopular(req, res, moviedb);
-});
-
-app.get("/movie/top_rated", async (req, res) => {
-	movieTopRated.handleMovieTopRated(req, res, moviedb);
-});
-
-app.get("/movie/upcoming", async (req, res) => {
-	movieUpcoming.handleMovieUpcoming(req, res, moviedb);
+app.get("/movielist/:sorted", async (req, res) => {
+	movieList.handleMovieList(req, res, moviedb);
 });
 
 app.listen(process.env.PORT || 3001, () => {
